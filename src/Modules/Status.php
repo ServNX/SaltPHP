510f93ce6177de5e053cac500daeb1ea9b423f94
poppabear <servnx@gmail.com>
2017-03-21 22:15:39 -0400

    Added the ability to return results specifically for a target

--- src/Modules/Status.php
+++ src/Modules/Status.php
@@ -7,13 +7,34 @@ class Status extends BaseModule
     public function cpuinfo($target = '*', $searchkey = null)
     {
         $this->salt->execute('status.cpuinfo', $target);
-        return ($searchkey !== null) ? $this->get($searchkey) : $this->getResults();
+
+        return $this->checkArgumentsAndGetResults($target, $searchkey);
+
     }
 
     public function loadavg($target = '*', $searchkey = null)
     {
         $this->salt->execute('status.loadavg', $target);
-        return ($searchkey !== null) ? $this->get($searchkey) : $this->getResults();
+
+        return $this->checkArgumentsAndGetResults($target, $searchkey);
+    }
+
+    /**
+     * @param $target
+     * @param $searchkey
+     * @return mixed
+     */
+    private function checkArgumentsAndGetResults($target, $searchkey)
+    {
+        if ($searchkey !== null) {
+            return $this->get($searchkey);
+        }
+
+        if ($target == '*') {
+            return $this->getResults();
+        }
+
+        return $this->getResults()[$target];
     }
 }
 
